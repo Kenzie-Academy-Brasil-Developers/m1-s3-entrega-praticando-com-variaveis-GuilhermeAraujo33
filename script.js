2eebd616f7f848cf7f43a585abeffd988b374eb8
Guilherme Rodrigues <guilherme.rodrigues.araujo17@gmail.com>
2022-06-14 13:22:47 -0300

    entrega tarefa S3-11

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = "Guilherme"
+let sobrenome = "Araujo"
+let anoNascimento = 2003
+let cidade = "São Paulo"
+let ano = 2022
+let idade = ano - anoNascimento;
+console.log("Eu sou " + nome +" "+ sobrenome + " tenho " + idade + " anos e moro em " +  cidade);
